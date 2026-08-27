1f2e1c121757cc1191b9e60d600f9eaf5b3e4963
murali-gude <gudemurali13@gmail.com>
2026-08-27 23:05:11 +0530

    Added new Java programs

--- javaBasics.java
+++ javaBasics.java
@@ -1,6 +1,6 @@
 
 class javaBasics {
     public static void main(String[] args) {
-        System.out.println("Hello Everyone");
+        System.out.println("Hello World welcome to programming");
     }
 }
