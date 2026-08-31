8422a0321a3c93b5ea92225b1b73e54796350155
murali-gude <gudemurali13@gmail.com>
2026-08-31 22:21:53 +0530

    largest number

---
+++ largestNumber.java
@@ -0,0 +1,19 @@
+public class largestNumber {
+
+    static int largest (int a, int b){
+        if (a>b){
+            return a;
+        } else if (b>a) {
+            return b;
+        } else {
+            return a; // or return b; since they are equal
+        }}
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = largest(num1, num2);
+        System.out.println("The largest number is: " + result);
+    }
+    
+}
