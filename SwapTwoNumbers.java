23e8f31a6ee472fc178c7872fe36a54d7a6bf77f
murali-gude <gudemurali13@gmail.com>
2026-08-31 22:13:55 +0530

    swapping number

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,3 +1,16 @@
 public class SwapTwoNumbers {
 
+    public static void main(String[] args) {
+        int a=15;
+        int b=20;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+        
+
+    }
+
 }
