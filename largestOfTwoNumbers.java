16b836634f1c727c6504904456e88d85dadff170
murali-gude <gudemurali13@gmail.com>
2026-09-01 22:48:25 +0530

    Added new Java programs

--- largestOfTwoNumbers.java
+++ largestOfTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class largestOfTwoNumbers {
-    public static void main(String[]args){
+    /*public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter A");
         int a = sc.nextInt();
@@ -15,12 +15,22 @@ public class largestOfTwoNumbers {
             System.out.println("B is largest");
         } else {
             System.out.println("C is largest");
+        }*/
+
+    static int largeNumbers(int a, int b, int c){
+        if (a >= b && a >= c) {
+            System.out.println("A is largest");
+            return a;
+        } else if (b >= a && b >= c) {
+            System.out.println("B is largest");
+            return b;
+        } else {
+            System.out.println("C is largest");
+            return c;
         }
-
-
     }
 
-
-
-
+    public static void main(String[]args){
+        largeNumbers(10,25,88);
+    }
 }
