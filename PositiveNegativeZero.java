53bef566e7cd2b703a57af6d42816e423b706fa3
murali-gude <gudemurali13@gmail.com>
2026-09-02 22:16:28 +0530

    added new code

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,39 @@
+public class PositiveNegativeZero {
+
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number to check positive, Negative or Zero");
+         int num = sc.nextInt();
+        
+         if(num ==0){
+            System.out.println("Given number is Zero");
+         }
+         else if(num>0){
+            System.out.println("Given number is Positive");
+         }
+         else{
+            System.out.println("Given number is Negative");
+}}*/
+        
+
+        static void  PNO(int num){
+            if(num ==0){
+            System.out.println("Given number is Zero");
+         }
+         else if(num>0){
+            System.out.println("Given number is Positive");
+         }
+         else{
+            System.out.println("Given number is Negative");
+            
+
+        }}
+
+        public static void main(String[]args){
+            PNO(-7);
+        }
+
+    
+
+}
