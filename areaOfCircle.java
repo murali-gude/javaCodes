11699bf5bfebb604054600b836903925daec59d2
murali-gude <gudemurali13@gmail.com>
2026-09-03 22:07:43 +0530

    Added new Java programs

---
+++ areaOfCircle.java
@@ -0,0 +1,10 @@
+public class areaOfCircle {
+    public static void main(String[]args){
+        int radius = 5;
+
+        double area = Math.round(Math.PI * radius * radius);
+
+        System.out.println(area + " area of circle");
+    }
+
+}
