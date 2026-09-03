3fedf14ffaf5f15a3dbbcad218a92f3b855fd4ad
murali-gude <gudemurali13@gmail.com>
2026-09-03 22:57:10 +0530

    Added new Java programs

---
+++ level 2/numbersPrinting.java
@@ -0,0 +1,36 @@
+public class numbersPrinting {
+
+    public static void main(String[] args) {
+// for loop  
+        /*for (int i =0; i<=100; i++){
+            System.out.println(i);
+        }
+// While Loop
+        int i =0;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+
+        }
+// do while
+       int i=0;
+        do { 
+            System.out.println(i);
+            i++;
+            
+        } while (i<=20);
+         */
+
+// Method
+        static int loop(){
+            for (int i =0; i<=100; i++){
+            System.out.println(i);
+        }
+        }
+        public static void main(String[] args) {
+            loop();
+        }
+
+    }
+
+}
