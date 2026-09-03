052148249c1b5085f71678832fcd9970ee7f6944
murali-gude <gudemurali13@gmail.com>
2026-09-03 21:50:10 +0530

    Added new Java programs

--- simpleInterest.java
+++ simpleInterest.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class simpleInterest {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+       /*Scanner sc = new Scanner(System.in);
         System.out.println("Enter Principle Amount");
          int Principle = sc.nextInt();
         System.out.println("Enter Time Period");
@@ -12,9 +12,17 @@ public class simpleInterest {
 
         int simpleInterst = ((Principle * Time * Rate)/100);
         System.out.println(simpleInterst + " total Simple Interest");
+    }*/
 
+        static int si( int p, int r, int t){
+            int simpleInterst = ((Principle * Time * Rate)/100);
+            System.out.println(simpleInterst + " total Simple Interest");
+            return simpleInterst;
+        }
 
-        
-    }
+        public static void main(String [] args){
+            int simpleInterst = si(1000,14,2);
+            System.out.println(simpleInterst + " total Simple Interest");
+        }
 
 }
